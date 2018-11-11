a6953787c212ae892a47c849dca90d2d086a21f1
zsy <530822987@qq.com>
2018-11-11 15:46:34 +0800

    add bootstrap use blade template

--- public/themes/original/views/index.blade.php
+++ public/themes/original/views/index.blade.php
@@ -1,18 +1,14 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    @yield('css')
-    {{Html::style('css/normalize.css')}}
-    {{Html::style('css/style.css')}}
-</head>
-<body>
-@include('partials._head')
-main page
-</body>
-</html>
+@extends('layouts.main')
 
+@section('css')
+    {{Html::style('css/swiper/swiper.min.css')}}
+@endsection
+
+
+@section('title')
+    {{env('APP_NAME', '浙江财经大学东方学院|东方园区')}}
+@endsection
+
+@section('body')
+    this is the body
+@endsection
--- public/themes/original/views/layouts/main.blade.php
+++ public/themes/original/views/layouts/main.blade.php
@@ -5,9 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title')</title>
+    {{Html::style('css/bootstrap.min.css')}}
+    @yield('css')
+    {{Html::style('css/normalize.css')}}
+    {{Html::style('css/style.css')}}
 </head>
 <body>
-
+@include('partials._head')
+<div class="container miancontent">
+    @yield('body')
+</div>
 </body>
 </html>

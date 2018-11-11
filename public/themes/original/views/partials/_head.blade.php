3e91966e4b76cae52a18fa78064ba6ce18ad083c
zsy <530822987@qq.com>
2018-11-11 17:36:56 +0800

    top zone

--- public/themes/original/views/partials/_head.blade.php
+++ public/themes/original/views/partials/_head.blade.php
@@ -1,6 +1,6 @@
-<div class="container">
+<div class="top-zone">
     <div>
-        <div class="top-zone">
+        <div class="">
             <a href="{{route('index')}}" title="{{env('APP_NAME')}}"></a>
             <div class="header_contact">
                 <a href="{{route('index')}}" title="联系我们" class="current">联系我们</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="{{route('index')}}" title="主网站">东方主站</a>

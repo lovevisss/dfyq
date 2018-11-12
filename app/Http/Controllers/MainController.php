5cd8c078364307318ef0f2a11c0ee6b01a250e3c
zsy <530822987@qq.com>
2018-11-12 20:45:23 +0800

    fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,7 +7,7 @@ class MainController extends Controller
 {
     public function index()
     {
-        $parent_menu = MenuItem::where('id', '=', 1)->first();
+        $parent_menu = MenuItem::where('id', '=', 15)->first();
         return view('index', compact('parent_menu'));
     }
 }

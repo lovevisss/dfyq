dbbebed64e9d535a1573c4eb18931334bb1b9317
zsy <530822987@qq.com>
2018-11-13 14:27:33 +0800

    swiper slider fix

--- app/Http/Composer/SliderComposer.php
+++ app/Http/Composer/SliderComposer.php
@@ -10,7 +10,7 @@ namespace App\Http\Composer;
 
 use Illuminate\Contracts\View\View;
 use TCG\Voyager\Models\Post;
-class NavComposer {
+class SliderComposer {
     public function compose(View $view)
     {
         $view->with('image_posts', Post::where('image' , '!=', null)->orderBy('created_at', 'DESC')->take(3)->get());

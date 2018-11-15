b83f23f3c5e415745c34a6d989b1935cf4883b27
zsy <530822987@qq.com>
2018-11-15 20:58:23 +0800

    page fix

--- public/themes/original/views/menus/_menu.blade.php
+++ public/themes/original/views/menus/_menu.blade.php
@@ -16,7 +16,20 @@
                 </li>
             @endforeach
    @else
-            {{dd($options)}}
+        @foreach($items as $key => $menu_item)
+            <li class="@if($menu_item->title == $title)
+                    current
+@endif"><a class="navName" href={{$menu_item->link()}} >{{ $menu_item->title }}</a>
+                @if($menu_item->children)
+                    <ul>
+                        @foreach($menu_item->children as $child)
+                            <li><a href={{$child->link()}}>{{$child->title}}</a>	</li>
+                        @endforeach
+                    </ul>
+
+                @endif
+            </li>
+        @endforeach
 
     @endif
 </ul>

78b55e85fb2b104afec72458851c128c2f9dbf43
zsy <530822987@qq.com>
2018-11-15 21:04:08 +0800

    page fix

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,7 +10,8 @@ class MainController extends Controller
     public function index()
     {
         $parent_menu = MenuItem::where('id', '=', 15)->first();
-        return view('index', compact('parent_menu'));
+        $title = 'tesf';
+        return view('index', compact('parent_menu','title'));
     }
 
     public function get_celebrity()

d629f11f4c47cf46a05d0d3c12499c5fcd6d19eb
zsy <530822987@qq.com>
2018-11-16 09:51:21 +0800

    title test

--- public/themes/original/views/partials/_nav.blade.php
+++ public/themes/original/views/partials/_nav.blade.php
@@ -1,5 +1,6 @@
 <div class="nav_w">
     <div class="nav navTwo">
-        {{menu('main', 'menus._menu', ['parent_menu' => $parent_menu, 'title' => $title])}}
+        {{--{{menu('main', 'menus._menu', ['parent_menu' => $parent_menu, 'title' => $title])}}--}}
+        {{dd($title)}}
     </div>
 </div>

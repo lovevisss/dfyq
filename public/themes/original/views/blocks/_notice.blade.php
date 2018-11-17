e635653db4bb4fd749ce840e5fb350f4f0eb54e8
zsy <530822987@qq.com>
2018-11-17 14:12:22 +0800

    test

--- public/themes/original/views/blocks/_notice.blade.php
+++ public/themes/original/views/blocks/_notice.blade.php
@@ -1,31 +1,33 @@
-@extends('layouts.partlist')
+{{--@extends('layouts.partlist')--}}
 
 
-@section('outerclass')
-    col-sm-6  small_list
-@stop
+{{--@section('outerclass')--}}
+    {{--col-sm-6  small_list--}}
+{{--@stop--}}
 
-@section('title')
+{{--@section('title')--}}
     {{--{{$notice_posts[0]->category->name}}--}}
-@endsection
+{{--@endsection--}}
 
-@section('link')
-    {{route('category', ['id' => 1])}}
-@endsection
+{{--@section('link')--}}
+    {{--{{route('category', ['id' => 1])}}--}}
+{{--@endsection--}}
 
-@section('list')
-    @foreach($notice_posts as $new)
-        <li>
-            <a href="post/{{$new->id}}" title="{{$new->title}}">
-                <i class="glyphicon glyphicon-triangle-right"></i>
-                <strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}
-                    @if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)
-                        <span class="new_info">new</span>
-                    @endif
-                </strong>
-                <span>{{date_format($new->created_at,"m-d")}}</span>
-            </a>
-        </li>
-        <div class="clearfix">	</div>
-    @endforeach
-@stop
+{{--@section('list')--}}
+    {{--@foreach($notice_posts as $new)--}}
+        {{--<li>--}}
+            {{--<a href="post/{{$new->id}}" title="{{$new->title}}">--}}
+                {{--<i class="glyphicon glyphicon-triangle-right"></i>--}}
+                {{--<strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}--}}
+                    {{--@if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)--}}
+                        {{--<span class="new_info">new</span>--}}
+                    {{--@endif--}}
+                {{--</strong>--}}
+                {{--<span>{{date_format($new->created_at,"m-d")}}</span>--}}
+            {{--</a>--}}
+        {{--</li>--}}
+        {{--<div class="clearfix">	</div>--}}
+    {{--@endforeach--}}
+{{--@stop--}}
+
+{{dd($notice_posts)}}

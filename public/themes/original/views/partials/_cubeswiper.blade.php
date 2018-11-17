87bb631998d3c4be5d0740147ab6c9dcd9389b7a
zsy <530822987@qq.com>
2018-11-17 10:37:35 +0800

    index swiper setup add image

--- public/themes/original/views/partials/_cubeswiper.blade.php
+++ public/themes/original/views/partials/_cubeswiper.blade.php
@@ -1,15 +1,12 @@
 <div class="cubeswiper-container">
     <div class="swiper-wrapper">
-        <div class="swiper-slide cube" style="background-image:url(img/nature1.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature2.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature3.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature4.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature5.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature6.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature7.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature8.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature9.jpg)"></div>
-        <div class="swiper-slide cube" style="background-image:url(img/nature10.jpg)"></div>
+
+        @foreach($sceneries as $scenery)
+
+        <div class="swiper-slide cube" style="background-image:url({{'storage/'.$scenery->image}})"></div>
+
+        @endforeach
+
     </div>
     <!-- Add Pagination -->
     <div class="swiper-pagination"></div>

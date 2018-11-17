acd97bed011e5bd8754cab18efdd0c136ea86988
zsy <530822987@qq.com>
2018-11-17 14:36:06 +0800

    bring back

--- public/themes/original/views/blocks/_notice.blade.php
+++ public/themes/original/views/blocks/_notice.blade.php
@@ -1,33 +1,31 @@
-{{--@extends('layouts.partlist')--}}
+@extends('layouts.partlist')
 
 
-{{--@section('outerclass')--}}
-    {{--col-sm-6  small_list--}}
-{{--@stop--}}
+@section('outerclass')
+    col-sm-6  small_list
+@stop
 
-{{--@section('title')--}}
-    {{--{{$notice_posts[0]->category->name}}--}}
-{{--@endsection--}}
+@section('title')
+    {{$notice_posts[0]->category->name}}
+@endsection
 
-{{--@section('link')--}}
-    {{--{{route('category', ['id' => 1])}}--}}
-{{--@endsection--}}
+@section('link')
+    {{route('category', ['id' => 1])}}
+@endsection
 
-{{--@section('list')--}}
-    {{--@foreach($notice_posts as $new)--}}
-        {{--<li>--}}
-            {{--<a href="post/{{$new->id}}" title="{{$new->title}}">--}}
-                {{--<i class="glyphicon glyphicon-triangle-right"></i>--}}
-                {{--<strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}--}}
-                    {{--@if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)--}}
-                        {{--<span class="new_info">new</span>--}}
-                    {{--@endif--}}
-                {{--</strong>--}}
-                {{--<span>{{date_format($new->created_at,"m-d")}}</span>--}}
-            {{--</a>--}}
-        {{--</li>--}}
-        {{--<div class="clearfix">	</div>--}}
-    {{--@endforeach--}}
-{{--@stop--}}
-
-{{dd($notice_posts[0])}}
+@section('list')
+    @foreach($notice_posts as $new)
+        <li>
+            <a href="post/{{$new->id}}" title="{{$new->title}}">
+                <i class="glyphicon glyphicon-triangle-right"></i>
+                <strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}
+                    @if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)
+                        <span class="new_info">new</span>
+                    @endif
+                </strong>
+                <span>{{date_format($new->created_at,"m-d")}}</span>
+            </a>
+        </li>
+        <div class="clearfix">	</div>
+    @endforeach
+@stop

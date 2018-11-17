e9855fe617c4984b9c5a12205a269bc68b60ca4c
zsy <530822987@qq.com>
2018-11-17 10:28:32 +0800

    index swiper setup

--- public/themes/original/views/index.blade.php
+++ public/themes/original/views/index.blade.php
@@ -36,6 +36,25 @@
                 delay: 3000,
             },
         })
+        var cubeswiper = new Swiper('.cubeswiper-container', {
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows : true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            autoplay:{
+                delay: 2000,
+            },
+        });
 
 
     </script>

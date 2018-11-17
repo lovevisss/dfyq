11b54c2f477c9beac46c7b4921a75ce0536072ef
zsy <530822987@qq.com>
2018-11-17 15:18:32 +0800

    new style for list news

--- public/themes/original/views/blocks/_notice.blade.php
+++ public/themes/original/views/blocks/_notice.blade.php
@@ -15,16 +15,21 @@
 
 @section('list')
     @foreach($notice_posts as $new)
-        <li>
-            <a href="post/{{$new->id}}" title="{{$new->title}}">
+
+        <li class="">
+            <span class="post-newday">26</span><span class="post-newmonth">2018/06</span>
+            <span class="ftitle"><a href="post/{{$new->id}}" title="{{$new->title}}">
                 <i class="glyphicon glyphicon-triangle-right"></i>
                 <strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}
                     @if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)
                         <span class="new_info">new</span>
                     @endif
                 </strong>
-                <span>{{date_format($new->created_at,"m-d")}}</span>
-            </a>
+            </a></span>
+            <span class="fbody"><p>{{App\Helper\StringHelper::substrtitle($new->body,20)}}</p></span>
+        </li>
+        <li>
+
         </li>
         <div class="clearfix">	</div>
     @endforeach

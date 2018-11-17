f181580532680f5c0442654de362fe789e071783
zsy <530822987@qq.com>
2018-11-17 13:54:50 +0800

    add css and template

--- public/themes/original/views/blocks/_notice.blade.php
+++ public/themes/original/views/blocks/_notice.blade.php
@@ -1,2 +1,27 @@
-this is notice area
+@extends('layouts.partlist')
 
+
+@section('outerclass')
+    col-sm-6  small_list
+@stop
+
+@section('link')
+    {{route('category', ['id' => 1])}}
+@endsection
+
+@section('list')
+    @foreach($notice_posts as $new)
+        <li>
+            <a href="post/{{$new->id}}" title="{{$new->title}}">
+                <i class="glyphicon glyphicon-triangle-right"></i>
+                <strong>{{App\Helper\StringHelper::substrtitle($new->title,10)}}
+                    @if($new->created_at->diffInHours(\Carbon\Carbon::now()) < 72)
+                        <span class="new_info">new</span>
+                    @endif
+                </strong>
+                <span>{{date_format($new->created_at,"m-d")}}</span>
+            </a>
+        </li>
+        <div class="clearfix">	</div>
+    @endforeach
+@stop

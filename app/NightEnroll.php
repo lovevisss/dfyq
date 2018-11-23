63902159a075a010d019527de7c508d07fde48c2
zsy <530822987@qq.com>
2018-11-23 09:19:54 +0800

    test one to one relation fix

--- app/NightEnroll.php
+++ app/NightEnroll.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class NightEnroll extends Model
 {
     public function user(){
-        $this->belongsTo('App\User');
+        return $this->belongsTo('App\User');
     }
 }

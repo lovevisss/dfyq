571cac2f7acddefd113bd1dd42526100c1379539
zsy <530822987@qq.com>
2018-12-06 20:36:47 +0800

    add Major and seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(DepartmentsTableSeeder::class);
+         $this->call(MajorsTableSeeder::class);
     }
 }

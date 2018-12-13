d721f355b7d3829e70969b2e01109f799bb271ac
zsy <530822987@qq.com>
2018-12-13 10:29:15 +0800

    add login template

---
+++ public/themes/original/views/layouts/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    {{Html::style('css/bootstrap.min.css')}}
+    @yield('css')
+    {{Html::style('css/normalize.css')}}
+    {{Html::style('css/style.css')}}
+    {{Html::style('css/login.css')}}
+    {{Html::style('css/datepicker.css')}}
+</head>
+<body>
+@yield('body')
+@include('partials._foot')
+</body>
+{{Html::script('js/jquery-1.11.1.min.js')}}
+	{{Html::script('js/main.js')}}
+	<!--[if lt IE 9]>
+　 <script src="https://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.js"></script>
+<![endif]-->
+@yield('script')
+</html>

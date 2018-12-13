0d3626f6d18a7bb4e87f0add0ccc23b8d2d68adf
zsy <530822987@qq.com>
2018-12-13 15:55:11 +0800

    password hash

--- app/User.php
+++ app/User.php
@@ -28,6 +28,11 @@ class User extends \TCG\Voyager\Models\User
         'password', 'remember_token',
     ];
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = \Hash::make($password);
+    }
+
     public function nightenrolls()
     {
         return $this->belongsToMany('App\Nightenroll');

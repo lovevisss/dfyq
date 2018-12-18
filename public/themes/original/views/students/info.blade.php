d2a196ad8112ada223e183405958a362bb7d0490
zsy <530822987@qq.com>
2018-12-18 15:43:38 +0800

    add info blade

--- public/themes/original/views/students/info.blade.php
+++ public/themes/original/views/students/info.blade.php
@@ -10,6 +10,24 @@
             {!! Form::label('name', '名字:') !!}
             {!! Form::text('name', null, ['class' => 'form-control']) !!}
         </div>
+        <div class="form-group">
+             {!! Form::label('email','Email:') !!}
+             {!! Form::text('email', null, ['class' => 'form-control']) !!}
+        </div>
+    
+        <div class="form-group">
+              {!! Form::label('student_num','学号:') !!}
+              {!! Form::text('student_num', null, ['class' => 'form-control']) !!}
+        </div>
+         <div class="form-group">
+             {!! Form::label('phone','手机:') !!}
+             {!! Form::('phone', null, ['class' => 'form-control']) !!}
+         </div>
+          <div class="form-group">
+              {!! Form::label('student_card','学生证:') !!}
+              {!! Form::file('student_card', null, ['class' => 'form-control']) !!}
+          </div>
+          {!! Form::submit(更新,['class' => 'btn btn-primary']) !!}
     {!! Form::close()!!}
 
 @endsection

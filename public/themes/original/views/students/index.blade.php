4ab3266dbba3b892d17e5c796c375dcd9ff6ba78
zsy <530822987@qq.com>
2018-12-19 15:39:57 +0800

    info update route fix

--- public/themes/original/views/students/index.blade.php
+++ public/themes/original/views/students/index.blade.php
@@ -7,7 +7,7 @@
     index
     {{Auth::user()}}
     <div class="image-container">
-        <img src="{{$student->student_card}}" alt="" style="width:300px; height:300px;">
+        <img src="{{base_path().$student->student_card}}" alt="" style="width:300px; height:300px;">
     </div>
     <ul>
         <li><a href="{{route('student.info')}}">我的资料</a></li>

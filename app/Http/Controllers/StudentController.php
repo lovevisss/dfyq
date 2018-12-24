7c513b8c36e4747a6148d6b178a89ce432c4c463
zsy <530822987@qq.com>
2018-12-24 16:14:07 +0800

    test session delete

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -21,7 +21,7 @@ class StudentController extends Controller
 //        $request->session()->put(['zsy' => 'author']);
         session(['yy' => 'data']);
         session()->forget('yy');
-        return $request->session();
+        return $request->session()->all();
         $parent_menu = MenuItem::where('id', '=', 15)->first();
         $student = Auth::user();
         return view('students.index',compact('parent_menu', 'student'));

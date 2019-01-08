786320c7d649a92a22475c8b974d2c2a67dc7fb6
zsy <530822987@qq.com>
2019-01-08 10:44:14 +0800

    accordian

---
+++ public/themes/original/views/layouts/student_index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('css')
+
+
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    {{Html::style('css/accordian.css')}}
+    @stop
+
+
+
+@section('script')
+
+
+    <script src='http://codepen.io/assets/libs/fullpage/jquery.js'></script>
+    {{Html::script('js/accordian.js')}}
+    @stop
+

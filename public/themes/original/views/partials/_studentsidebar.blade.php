a134d73e8f63be34310ef20dc29f880301a2914b
zsy <530822987@qq.com>
2019-01-09 10:14:32 +0800

    accordian

---
+++ public/themes/original/views/partials/_studentsidebar.blade.php
@@ -0,0 +1,36 @@
+<ul id="accordion" class="accordion">
+    <li>
+        <div class="link"><i class="fa fa-paint-brush"></i>个人信息<i class="fa fa-chevron-down"></i></div>
+        <ul class="submenu">
+            <li><a href="#">基本信息</a></li>
+            <li><a href="#">密码更新</a></li>
+            <li><a href="#">资格认证</a></li>
+            {{--<li><a href="#">Maquetacion web</a></li>--}}
+        </ul>
+    </li>
+    <li>
+        <div class="link"><i class="fa fa-code"></i>商品信息<i class="fa fa-chevron-down"></i></div>
+        <ul class="submenu">
+            <li><a href="#">已登记商品</a></li>
+            <li><a href="#">登记新商品</a></li>
+            {{--<li><a href="#">Frameworks javascript</a></li>--}}
+        </ul>
+    </li>
+    <li>
+        <div class="link"><i class="fa fa-mobile"></i>夜市报名<i class="fa fa-chevron-down"></i></div>
+        <ul class="submenu">
+            <li><a href="#">已报名夜市</a></li>
+            <li><a href="#">查看夜市</a></li>
+            {{--<li><a href="#">Medios de escritorio</a></li>--}}
+            {{--<li><a href="#">Otros dispositivos</a></li>--}}
+        </ul>
+    </li>
+    <li><div class="link"><i class="fa fa-globe"></i>Posicionamiento web<i class="fa fa-chevron-down"></i></div>
+        <ul class="submenu">
+            <li><a href="#">Google</a></li>
+            <li><a href="#">Bing</a></li>
+            <li><a href="#">Yahoo</a></li>
+            <li><a href="#">Otros buscadores</a></li>
+        </ul>
+    </li>
+</ul>

acd7b04f492dd378d9a3558cf430608d0613e6ef
zsy <530822987@qq.com>
2019-01-09 10:46:24 +0800

    accordian

--- public/themes/original/views/students/index.blade.php
+++ public/themes/original/views/students/index.blade.php
@@ -9,15 +9,15 @@
         @include('partials._studentsidebar')
     </div>
     <div class="col-sm-7">
-        {{Auth::user()}}
-        <div class="image-container">
-            <img src="{{$student->student_card}}" alt="" style="width:300px; height:300px;">
-        </div>
-        <ul>
-            <li><a href="{{route('student.info')}}">我的资料</a></li>
-            <li></li>
-            <li></li>
-        </ul>
+        @include('students.partials.info')
+        {{--<div class="image-container">--}}
+            {{--<img src="{{$student->student_card}}" alt="" style="width:300px; height:300px;">--}}
+        {{--</div>--}}
+        {{--<ul>--}}
+            {{--<li><a href="{{route('student.info')}}">我的资料</a></li>--}}
+            {{--<li></li>--}}
+            {{--<li></li>--}}
+        {{--</ul>--}}
     </div>
 
 
---
+++ public/themes/original/views/students/partials/info.blade.php
@@ -0,0 +1,26 @@
+{!! Form::model($student, ['url' => route('student.update'), 'files' => true])!!}
+
+<div class="form-group">
+    {!! Form::label('name', '名字:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('email','Email:') !!}
+    {!! Form::text('email', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('student_num','学号:') !!}
+    {!! Form::text('student_num', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('phone','手机:') !!}
+    {!! Form::text('phone', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('student_card','学生证:') !!}
+    {!! Form::file('student_card', null, ['class' => 'form-control']) !!}
+</div>
+
+{!! Form::submit('更新',['class' => 'btn btn-primary']) !!}
+{!! Form::close()!!}

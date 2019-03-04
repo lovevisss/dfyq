e30b9395d2dd64252151eb7dd05233e38297148e
zsy <530822987@qq.com>
2019-03-04 13:33:55 +0800

    short check of student card

--- public/themes/original/views/students/info.blade.php
+++ public/themes/original/views/students/info.blade.php
@@ -25,7 +25,7 @@
          </div>
           <div class="form-group">
               {!! Form::label('student_card','学生证:') !!}
-              {!! Form::file('student_card', null, ['class' => 'form-control']) !!}
+              {!! Form::file('student_card',  ['class' => 'form-control']) !!}
           </div>
 
           {!! Form::submit('更新',['class' => 'btn btn-primary']) !!}
